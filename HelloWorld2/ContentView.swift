85cd3e5d77379751e22f1fa3ff7491bb4bb9e133
Rmccoy0502 <s0323792@student.rockvalleycollege.edu>
2023-09-02 23:47:44 -0500

    Initial Commit

---
+++ HelloWorld2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld2
+//
+//  Created by mac user on 9/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Rio!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld2/HelloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld2App.swift
+//  HelloWorld2
+//
+//  Created by mac user on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
